b52be8f1428b615aeea020e0f5094c11a7665e0d
zqb <zhangqianbin3@foxmail.com>
2019-04-18 23:42:42 +0800

    自定义主题

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,48 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    final appName = '自定义主题';
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+        title: appName,
+        theme: new ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.lightGreen[600],
+          accentColor: Colors.orange[600],
         ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+        home: new MyHomePage(
+          title: appName,
+        ));
   }
 }
 
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, @required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new Container(
+          color: Theme.of(context).accentColor,
+          child: new Text(
+            '带有背景颜色的文本组件',
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
+      ),
+      floatingActionButton: new Theme(
+          data: Theme.of(context).copyWith(accentColor: Colors.grey),
+          child: new FloatingActionButton(
+            onPressed: null,
+            child: new Icon(Icons.computer),
+          )),
+    );
+  }
+}
