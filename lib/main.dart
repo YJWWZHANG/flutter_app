b31badb6bcbf1addf9950c1cf9158f2a6028b0b6
zqb <zhangqianbin3@foxmail.com>
2019-04-20 01:51:34 +0800

    httpclient请求

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+
+  void getWeatherData() async{
+    try {
+      var httpClient = new HttpClient();
+      var httpClientRequest = await httpClient.getUrl(Uri.parse("http://t.weather.sojson.com/api/weather/city/101030100"));
+      var httpClientResponse = await httpClientRequest.close();
+      var s = await httpClientResponse.transform(utf8.decoder).join();
+      print(s);
+      httpClient.close();
+    } catch(e) {
+      print("请求失败：$e");
+    } finally {
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'http请求示例',
+      title: 'httpclient请求',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('http请求示例'),
+          title: new Text('httpclient请求'),
         ),
         body: new Center(
           child: new RaisedButton(
-            onPressed: () {
-              var url = 'http://httpbin.org/';
-              http.get(url).then((response) {
-                print("状态：${response.statusCode}");
-                print("正文：${response.body}");
-              });
-            },
-            child: new Text('发起http请求'),
+            onPressed: getWeatherData,
+            child: new Text('获取天气数据'),
           ),
         ),
       )
