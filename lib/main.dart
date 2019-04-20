c8aa41044202b9a04238e74b290c1b4af1f9cd8e
zqb <zhangqianbin3@foxmail.com>
2019-04-20 20:51:06 +0800

    文本控件

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image Demo',
-      home: new Center(
-        child: Image.network(
-          "https://img4.duitang.com/uploads/item/201208/30/20120830092541_GsL4y.jpeg",
-          fit: BoxFit.fill,
-        )
+      title: '文本控件',
+      home: Scaffold(
+        appBar: new AppBar(
+          title: new Text("文本控件"),
+        ),
+        body: new Column(
+          children: <Widget>[
+            new Text(
+              "红色+黑色删除块+25号",
+              style: new TextStyle(
+                color: const Color(0xffff0000),
+                decoration: TextDecoration.lineThrough,
+                decorationColor: const Color(0xff000000),
+                fontSize: 25.0
+              ),
+            ),
+            new Text(
+              "橙色+下划线+24号",
+              style: new TextStyle(
+                color: const Color(0xffff9900),
+                decoration: TextDecoration.underline,
+                fontSize: 24.0
+              ),
+            ),
+            new Text(
+              "虚线上划线+23号+倾斜",
+              style: new TextStyle(
+                decoration: TextDecoration.overline,
+                decorationStyle: TextDecorationStyle.dashed,
+                fontSize: 23.0,
+                fontStyle: FontStyle.italic
+              ),
+            ),
+            new Text(
+              "43号+加粗",
+              style: new TextStyle(
+                fontSize: 23.0,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 6.0
+              ),
+            ),
+          ],
+        ),
       )
     );
   }
