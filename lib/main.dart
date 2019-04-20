5fd761eda016a4be4bcf63e8b14c48f5f656f656
zqb <zhangqianbin3@foxmail.com>
2019-04-20 18:25:47 +0800

    Image Demo

--- lib/main.dart
+++ lib/main.dart
@@ -7,31 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '容器组件示例',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('容器组件示例'),
-        ),
-        body: new Center(
-          child: Container(
-            width: 200.0,
-            height: 200.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: new Border.all(
-                color: Colors.grey,
-                width: 8.0
-              ),
-              borderRadius:
-                const BorderRadius.all(const Radius.circular(8.0))
-            ),
-            child: Text(
-              'Flutter',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 28.0),
-            ),
-          ),
-        ),
+      title: 'Image Demo',
+      home: new Center(
+        child: Image.network(
+          "https://img4.duitang.com/uploads/item/201208/30/20120830092541_GsL4y.jpeg",
+          fit: BoxFit.fill,
+        )
       )
     );
   }
