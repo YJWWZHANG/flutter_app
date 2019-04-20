10ff6f562d6c6e37a94fa2d126ab1a2ac28c41fd
zqb <zhangqianbin3@foxmail.com>
2019-04-20 21:35:27 +0800

    图标组件示例

--- lib/main.dart
+++ lib/main.dart
@@ -7,50 +7,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '文本控件',
+      title: '图标组件示例',
       home: Scaffold(
         appBar: new AppBar(
-          title: new Text("文本控件"),
+          title: new Text("图标组件示例"),
         ),
-        body: new Column(
-          children: <Widget>[
-            new Text(
-              "红色+黑色删除块+25号",
-              style: new TextStyle(
-                color: const Color(0xffff0000),
-                decoration: TextDecoration.lineThrough,
-                decorationColor: const Color(0xff000000),
-                fontSize: 25.0
-              ),
-            ),
-            new Text(
-              "橙色+下划线+24号",
-              style: new TextStyle(
-                color: const Color(0xffff9900),
-                decoration: TextDecoration.underline,
-                fontSize: 24.0
-              ),
-            ),
-            new Text(
-              "虚线上划线+23号+倾斜",
-              style: new TextStyle(
-                decoration: TextDecoration.overline,
-                decorationStyle: TextDecorationStyle.dashed,
-                fontSize: 23.0,
-                fontStyle: FontStyle.italic
-              ),
-            ),
-            new Text(
-              "43号+加粗",
-              style: new TextStyle(
-                fontSize: 23.0,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 6.0
-              ),
-            ),
-          ],
-        ),
+        body: new Icon(
+          Icons.phone, color: Colors.green[500], size: 80.0,
+        )
       )
     );
   }
