f1ee9eebcf0fccc838d270a430b88d34d42bb14e
zqb <zhangqianbin3@foxmail.com>
2019-04-20 23:39:14 +0800

    Form表单示例

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,90 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp(
-  items: new List<String>.generate(500, (i) => "Item $i")
-));
+void main() => runApp(new LoginPage());
 
-class MyApp extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
 
-  final List<String> items;
+class _LoginPageState extends State<LoginPage> {
+  var loginKey = new GlobalKey<FormState>();
 
-  MyApp({Key key, @required this.items}) : super(key: key);
+  String userName;
+
+  String password;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '网络列表示例',
-      home: Scaffold(
+
+    return new MaterialApp(
+      title: "Form表单示例",
+      home: new Scaffold(
         appBar: new AppBar(
-          title: new Text("网络列表示例"),
+          title: new Text("Form表单示例"),
         ),
-        body: new GridView.count(
-          primary: false,
-          padding: EdgeInsets.all(20.0),
-          crossAxisSpacing: 30.0,
-          crossAxisCount: 3,
+        body: new Column(
           children: <Widget>[
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
-            const Text("AAAAAAAAAAAAAAA"),
+            new Container(
+              padding: EdgeInsets.all(16.0),
+              child: new Form(
+                key: loginKey,
+                child: new Column(
+                  children: <Widget>[
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "请输入用户名"
+                      ),
+                      onSaved: (value) {
+                        userName = value;
+                      },
+                      onFieldSubmitted: (value) {
+
+                      },
+                    ),
+                    new TextFormField(
+                      decoration: new InputDecoration(
+                        labelText: "请输入密码"
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        return value.length < 6 ? "密码长度不够6位" : null;
+                      },
+                      onSaved: (value) {
+                        password = value;
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+            new SizedBox(
+              width: 340.0,
+              height: 42.0,
+              child: RaisedButton(
+                onPressed: login,
+                child: new Text(
+                  "登录",
+                  style: TextStyle(
+                    fontSize: 18.0
+                  ),
+                ),
+              ),
+            )
           ],
-        )
-      )
+        ),
+      ),
     );
   }
+
+  void login() {
+    var loginForm = loginKey.currentState;
+    if (loginForm.validate()) {
+      loginForm.save();
+      print("userName:$userName password:$password");
+    }
+  }
 }
+
 
 
