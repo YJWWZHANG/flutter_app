32a6188f54fba39b8bc6290ffe41d9ca4e5203bd
zqb <zhangqianbin3@foxmail.com>
2019-04-20 21:42:03 +0800

    图标按钮组件示例

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '图标组件示例',
+      title: '图标按钮组件示例',
       home: Scaffold(
         appBar: new AppBar(
-          title: new Text("图标组件示例"),
+          title: new Text("图标按钮组件示例"),
         ),
-        body: new Icon(
-          Icons.phone, color: Colors.green[500], size: 80.0,
+        body: new Center(
+          child: new IconButton(
+              icon: Icon(Icons.volume_up, size: 48.0),
+              tooltip: "按下操作",
+              onPressed: () {
+                print("按下操作");
+              }
+          ),
         )
       )
     );
