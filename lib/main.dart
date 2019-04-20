6184faa5b74ec818d32f48d5f3e854f2c5921d1b
zqb <zhangqianbin3@foxmail.com>
2019-04-20 21:46:59 +0800

    RaisedButton示例

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '图标按钮组件示例',
+      title: 'RaisedButton示例',
       home: Scaffold(
         appBar: new AppBar(
-          title: new Text("图标按钮组件示例"),
+          title: new Text("RaisedButton示例"),
         ),
         body: new Center(
-          child: new IconButton(
-              icon: Icon(Icons.volume_up, size: 48.0),
-              tooltip: "按下操作",
-              onPressed: () {
-                print("按下操作");
-              }
-          ),
+          child: new RaisedButton(
+            onPressed: () {},
+            child: new Text("RaisedButton示例"),
+          )
         )
       )
     );
