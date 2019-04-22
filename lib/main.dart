a4bd2377c926eabc6e8cfcf9032a46b3a0ee94f3
zqb <zhangqianbin3@foxmail.com>
2019-04-23 00:55:25 +0800

    Drawer抽屉组件示例

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(TabBarSample());
+void main() => runApp(
+  MaterialApp(
+    title: "Drawer抽屉组件示例",
+    home: LayoutDemo(),
+  )
+);
 
-class TabBarSample extends StatelessWidget {
-
-  List<ItemView> items = <ItemView> [
-    ItemView(title: "自驾", icon: Icons.directions_car),
-    ItemView(title: "自行车", icon: Icons.directions_bike),
-    ItemView(title: "轮船", icon: Icons.directions_boat),
-    ItemView(title: "公交车", icon: Icons.directions_bus),
-    ItemView(title: "火车", icon: Icons.directions_railway),
-    ItemView(title: "步行", icon: Icons.directions_walk),
-  ];
-
+class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: items.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("TabBar选项卡示例"),
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: items.map((ItemView item) {
-                return Tab(
-                  text: item.title,
-                  icon: Icon(item.icon)
-                );
-              }).toList(),
-            ),
-          ),
-          body: TabBarView(
-            children: items.map((ItemView item) {
-              return Padding(
-                padding: EdgeInsets.all(16.0),
-                child: SelectedView(item: item),
-              );
-            }).toList(),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Drawer抽屉组件示例"),
       ),
-    );
-  }
-}
-
-class SelectedView extends StatelessWidget {
-  ItemView item;
-  SelectedView({Key key, this.item}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var testStyle = Theme.of(context).textTheme.display1;
-    return Card(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
           children: <Widget>[
-            Icon(item.icon, size: 128.0, color: testStyle.color),
-            Text(item.title, style: testStyle)
+            UserAccountsDrawerHeader(
+              accountName: Text("示例"),
+              accountEmail: Text("123@qq.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("images/1.jpeg"),
+              ),
+              onDetailsPressed: () {},
+              otherAccountsPictures: <Widget>[
+                Container(
+                  child: Image.asset("images/1.jpeg"),
+                )
+              ],
+            ),
+            ListTile(
+              leading: CircleAvatar(child: Icon(Icons.color_lens)),
+              title: Text("个性装扮"),
+            ),
+            ListTile(
+              leading: CircleAvatar(child: Icon(Icons.phone)),
+              title: Text("我的相册"),
+            ),
+            ListTile(
+              leading: CircleAvatar(child: Icon(Icons.wifi)),
+              title: Text("免流量特权"),
+            ),
           ],
         ),
       ),
     );
   }
-
-
 }
 
-class ItemView {
-  String title;
-  IconData icon;
-  ItemView({this.title, this.icon});
-}
 
 
 
