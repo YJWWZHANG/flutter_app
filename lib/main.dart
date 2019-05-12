8e273193aec5eb7b729a833c2af5b2760ccf856d
zqb <zhangqianbin3@foxmail.com>
2019-05-12 16:27:05 +0800

    FlatButton扁平按钮组件示例

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    title: "FloatingActionButton示例",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("FloatingActionButton示例"),
-      ),
-      body: Center(
-        child: Text(
-          "",
-          style: TextStyle(fontSize: 28.0),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Welcone to Flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("FlatButton扁平按钮组件示例"),
+        ),
+        body: Center(
+          child: FlatButton(
+              onPressed: () {},
+              child: Text(
+                "FlatButton",
+                style: TextStyle(
+                  fontSize: 24.0
+                ),
+              )
+          ),
         ),
       ),
-      floatingActionButton: Builder(
-        builder: (BuildContext context){
-          return FloatingActionButton(
-            child: Icon(Icons.add),
-            tooltip: "请点击FloatingActionButton",
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.blue,
-            elevation: 7.0,
-            highlightElevation: 14.0,
-            onPressed: () {
-              Scaffold.of(context).showSnackBar(
-                SnackBar(content: Text("你点击了FloatingActionButton"))
-              );
-            },
-            mini: false,
-            shape: CircleBorder(),
-            isExtended: false,
-          );
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    )
-  )
-);
+    );
+  }
+}
 
 
 
