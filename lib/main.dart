93175099a81df60e2bf39b0b11aeb76a7214c444
zqb <zhangqianbin3@foxmail.com>
2019-05-22 21:51:46 +0800

    +

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() => runApp(MaterialApp(
-  title: "Card布局示例",
-  home: MyApp(),
-));
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var card = SizedBox(
-      height: 250.0,
-      child: Card(
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              title: Text(
-                "深圳市南山区深南大道35号",
-                style: TextStyle(fontWeight: FontWeight.w300),
-              ),
-              subtitle: Text("创想科技有限公司"),
-              leading: Icon(Icons.home, color: Colors.lightBlue),
-            ),
-            Divider(),
-            ListTile(
-              title: Text(
-                "深圳市南山区深南大道32号",
-                style: TextStyle(fontWeight: FontWeight.w300),
-              ),
-              subtitle: Text("一木培训机构"),
-              leading: Icon(Icons.school, color: Colors.lightBlue),
-            ),
-            Divider(),
-          ],
+    return MaterialApp(
+      title: "CupertinoActivityIndicator示例",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("CupertinoActivityIndicator示例"),
         ),
-      ),
-    );
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Card布局示例"),
-      ),
-      body: Center(
-        child: card,
+        body: Center(
+          child: CupertinoActivityIndicator(
+            radius: 60.0,
+          ),
+        ),
       ),
     );
   }
